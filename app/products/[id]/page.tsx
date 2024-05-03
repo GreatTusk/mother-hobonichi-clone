8ffe5bfb512c98c8e7d57f369dbc3333ec69841e
F776 <fern.belmar@duocuc.cl>
2024-05-02 22:51:17 -0400

    created product pages

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import { fetchProductById } from "@/app/lib/data";
-import { ProductCarousel } from "@/app/ui/products/product-carousel";
+import ImageHeading from "@/app/ui/products/image-heading";
 
 export const metadata: Metadata = {
   title: "Edit invoice",
@@ -17,34 +17,9 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <main>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl px-4 pb-12 pt-24 lg:px-6">
-          <div className="mx-auto mb-8 max-w-screen-sm text-center">
-            <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white lg:text-4xl">
-              {product.name}
-            </h2>
-            <p className="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
-              We use an agile approach to test assumptions and connect with the
-              needs of your audience early and often.
-            </p>
-          </div>
-          <div className="mx-auto size-96 text-center">
-            <ProductCarousel image_url={product.image_url} />
-          </div>
-        </div>
-      </section>
-
-      {/*<Breadcrumbs*/}
-      {/*  breadcrumbs={[*/}
-      {/*    { label: 'Invoices', href: '/dashboard/invoices' },*/}
-      {/*    {*/}
-      {/*      label: 'Edit Invoice',*/}
-      {/*      href: `/dashboard/invoices/${id}/edit`,*/}
-      {/*      active: true,*/}
-      {/*    },*/}
-      {/*  ]}*/}
-      {/*/>*/}
-      {/*<Form invoice={invoice} customers={customers} />*/}
+      <div className="mx-auto max-w-screen-xl px-4 pb-12 pt-24 lg:px-6">
+        <ImageHeading product={product} />
+      </div>
     </main>
   );
 }
